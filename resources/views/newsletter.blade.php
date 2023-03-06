21258a4985942db757404521eba1c79bf80d83cd
vahnmarty <vahnmarty@gmail.com>
2023-03-06 14:09:33 +0800

    Mobile UI

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -16,14 +16,14 @@
         <div class="py-12 md:py-20 lg:py-20">
             <div class="px-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
                 <div class="grid gap-16 lg:grid-cols-2">
-                    <div class="flex flex-col">
+                    <div class="flex-col lg:flex">
                         <div>
                             <img src="{{ asset('img/logo.svg') }}" alt="" class="w-auto h-6 lg:h-auto">
                         </div>
 
-                        <h2 class="mt-16 text-4xl font-bold text-white lg:text-6xl">Sprouting Soon!</h2>
+                        <h2 class="mt-16 text-3xl font-bold text-white lg:text-6xl">Sprouting Soon!</h2>
 
-                        <h4 class="mt-8 text-xl font-bold leading-1 text-light-green lg:text-3xl">
+                        <h4 class="mt-8 text-lg font-bold leading-1 text-light-green lg:text-3xl">
                             Join our email list and get notified of when we launch eTanom
                             this 2023!
                         </h4>
@@ -43,13 +43,18 @@
 
                         <form action="{{ url('newsletter') }}" class="mt-8">
                             @csrf
-                            <div class="flex border border-light-green">
+                            <div class="border rounded-sm border-light-green lg:flex lg:rounded-none">
                                 <input type="email" name="email" autofocus
                                     class="flex-1 pl-4 text-white bg-transparent border-none placeholder:text-light-green hover:bg-light-green/20 focus:ring-1 focus:ring-inset focus:ring-light-green"
                                     placeholder="Enter your email">
-                                <button type="submit" class="px-8 bg-light-green hover:bg-light-green/90">Notify
+                                <button type="submit"
+                                    class="hidden px-8 bg-light-green hover:bg-light-green/90 lg:block">Notify
                                     Me!</button>
                             </div>
+
+                            <button
+                                type="submit"class="block w-full py-2 mt-4 text-center rounded-sm bg-light-green hover:bg-light-green/90 lg:hidden">Notify
+                                Me</button>
                         </form>
 
                         <div class="flex gap-4 mt-8 lg:mt-8">
