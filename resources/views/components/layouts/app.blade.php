c76f8a44dba0bc1510b6d3e880114d9852e188b5
vahnmarty <vahnmarty@gmail.com>
2023-08-30 11:19:45 +0800

    forms

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,17 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+        <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+        <title>{{ config('app.name') }}</title>
+ 
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+ 
+        @filamentStyles
+        @vite('resources/css/app.css')
     </head>
+ 
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
@@ -28,5 +34,10 @@
                 </div>
             </div>
         </div>
+
+
+        @filamentScripts
+        @vite('resources/js/app.js')
     </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+import preset from './vendor/filament/support/tailwind.config.preset'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    presets: [preset],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
 
     theme: {
