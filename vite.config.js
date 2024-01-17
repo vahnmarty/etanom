8b0fc20d5e63bef9b884ed8c69588454df086086
vahnmarty <vahnmarty@gmail.com>
2024-01-18 01:36:21 +0800

    add glide

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/custom.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/custom.js', 'resources/js/glide.js'],
             refresh: [
                 ...refreshPaths,
                 'app/Livewire/**',
