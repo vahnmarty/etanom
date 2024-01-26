38a63fc2af3f79ed077cedbb87efb21116b1cf65
vahnmarty <vahnmarty@gmail.com>
2024-01-26 15:22:02 +0800

    plant tree ui

--- app/Livewire/PlantTree.php
+++ app/Livewire/PlantTree.php
@@ -6,19 +6,27 @@ use Livewire\Component;
 
 class PlantTree extends Component
 {
-    public $trees_open = true;
+    public $show_trees = true, $show_planter = false, $show_cart = false;
 
-    public $show_planter = true;
+    public $plant, $planter, $quantity = 1;
 
     public function render()
     {
         return view('livewire.plant-tree')->layout('layouts.guest');
     }
 
-    public function select($tree)
+    public function selectTree($tree)
     {
         $this->show_planter = true;
-        $this->trees_open = false;
-        //$this->loading = true;
+        $this->show_trees = false;
+        $this->plant = $tree;
+    }
+
+    public function selectPlanter($planter)
+    {
+        $this->show_planter = false;
+        $this->show_trees = false;
+        $this->show_cart = true;
+        $this->planter = $planter;
     }
 }
--- resources/views/livewire/plant-tree.blade.php
+++ resources/views/livewire/plant-tree.blade.php
@@ -18,7 +18,7 @@
     </div>
     <div class="lg:max-w-5xl lg:mx-auto">
         
-        <section x-show="$wire.trees_open">
+        <section x-show="$wire.show_trees">
             <div class="relative lg:w-96">
                 <input type="search" 
                     class="block w-full pl-10 border border-gray-300 rounded-md"
@@ -33,7 +33,7 @@
         
             <div class="grid grid-cols-2 gap-4 mt-8 lg:grid-cols-4 lg:gap-8">
         
-                <div wire:click="select('narra')" class="shadow-lg cursor-pointer rounded-xl">
+                <div wire:click="selectTree('narra')" class="shadow-lg cursor-pointer rounded-xl">
                     <div class="p-4 bg-green-300 hover:bg-green-500 rounded-t-xl">
                         <img src="{{ asset('img/plants/narra.png') }}" class="w-auto h-32 mx-auto" alt="">
                     </div>
@@ -43,7 +43,7 @@
                     </div>
                 </div>
         
-                <div wire:click="select('talisay')" class="shadow-lg rounded-xl">
+                <div wire:click="selectTree('talisay')" class="shadow-lg rounded-xl">
                     <div class="p-4 bg-green-300 rounded-t-xl hover:bg-green-500">
                         <img src="{{ asset('img/plants/talisay.png') }}" class="w-auto h-32 mx-auto" alt="">
                     </div>
@@ -57,7 +57,74 @@
         </section>
 
         <section x-show="$wire.show_planter">
-            
+            <div>
+                <p class="text-gray-600">You have selected:</p>
+                <h4 class="text-2xl font-bold uppercase text-normal-green">{{ $plant}}</h4>
+            </div>
+
+            <div class="p-8 mt-8 text-white rounded-lg shadow-md bg-normal-green">
+                <p>We found 3 suitable planters to plant {{ $plant }} for you !</p>
+
+                <div class="mt-8 space-y-4">
+                    @foreach(range(1, 4) as $planter)
+                    <div wire:click="selectPlanter({{ $planter }})"
+                        class="flex gap-4 p-4 text-gray-600 bg-gray-100 border rounded-md shadow-sm cursor-pointer">
+                        <img src="{{ asset('img/avatar.png') }}" class="flex-shrink-0">
+                        <div class="flex-1">
+                            <p class="text-gray-700">Planter {{ $planter }}</p>
+                            <p class="text-sm">Location {{ $planter }}</p>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+
+        <section x-show="$wire.show_cart" class="space-y-8">
+            <div>
+                <p class="text-gray-600">Your Plant:</p>
+                <h4 class="text-2xl font-bold uppercase text-normal-green">{{ $plant}}</h4>
+            </div>
+            <div>
+                <p class="text-gray-600">Your Planter:</p>
+                <h4 class="text-2xl font-bold uppercase text-normal-green">Planter {{ $planter }}</h4>
+            </div>
+            <div>
+                <form action="">
+                    <div>
+                        <label for="">Enter number of tree(s)</label>
+                        <input type="number" pattern="[0-9]*" wire:model="quantity" class="block w-full mt-1 text-center border border-gray-300 rounded-md">
+                    </div>
+
+                    <div class="pt-4 pb-4 mt-8 border-t border-gray-300 border-dashed">
+                        <p class="font-bold text-gray-900">Summary</p>
+
+                        <div class="mt-4 space-y-2">
+                            <div class="flex justify-between">
+                                <span>Subtotal</span>
+                                <span>P350.00</span>
+                            </div>
+                            <div class="flex justify-between">
+                                <span>Transaction Fees</span>
+                                <span>P25.00</span>
+                            </div>
+                            <div class="flex justify-between">
+                                <span>VAT</span>
+                                <span>P5.00</span>
+                            </div>
+                            
+                            <div class="flex justify-between pt-4 border-t border-gray-300 border-dashed">
+                                <span class="font-bold">TOTAL</span>
+                                <span class="font-bold">P380.00</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-8">
+                        <button type="submit" class="block w-full py-3 text-gray-100 rounded-md shadow-sm bg-normal-green">Checkout</button>
+                    </div>
+                </form>
+            </div>
         </section>
     </div>
 </div>
